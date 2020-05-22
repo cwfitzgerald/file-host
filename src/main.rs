66da70abb08d17275ce65fd93a8d11574302665d
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-05-22 19:26:39 -0400

    Multipart data when

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,38 @@
-use rocket::routes;
+#![feature(decl_macro, proc_macro_hygiene)]
+
+use once_cell::sync::Lazy;
+use rocket::request::Form;
+use rocket::{post, routes, Data, FromForm};
 use rocket_contrib::serve::StaticFiles;
+use std::path::{Path, PathBuf};
+
+static UPLOAD_DIR: Lazy<PathBuf> = Lazy::new(|| {
+    let string = std::env::args()
+        .nth(1)
+        .expect("Must pass upload dir as first argument");
+    let absolute = Path::new(&string)
+        .canonicalize()
+        .expect("Directory must exist");
+    absolute
+});
+
+#[derive(FromForm)]
+struct UploadData {
+    extant: String,
+    data: String,
+}
+
+#[post("/upload", data = "<data>")]
+fn upload(data: Form<UploadData>) {
+    std::fs::write(
+        UPLOAD_DIR.join(String::from("test1.") + &data.extant),
+        &data.data,
+    );
+}
 
 fn main() {
-    rocket::ignite().mount("/", StaticFiles::from(std::env::args().skip(1).next().expect("Must pass upload dir as first argument"))).launch();
+    rocket::ignite()
+        .mount("/", StaticFiles::from(&*UPLOAD_DIR))
+        .mount("/", routes![upload])
+        .launch();
 }
